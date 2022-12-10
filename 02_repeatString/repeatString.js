886593ae246428c22624b40407e9de4615075224
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-10 16:26:20 +0300

    solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        let repeated = "";
+        for (let i = 0; i < number; i++) {
+            repeated = repeated.concat(string)
+        }
+        return repeated;
+    }
 };
 
 // Do not edit below this line
