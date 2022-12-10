e2a22c48ef2babf9b578542fdc0351ff8d3e1b6f
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-10 19:18:26 +0300

    solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    stringCharacters = string.split("");
+    const reversedString = [];
+    for (let i = 1; i <= stringCharacters.length; i++) {
+        reversedString.push(stringCharacters[stringCharacters.length - i]);
+    }
+    return reversedString.join("");
 };
 
 // Do not edit below this line
