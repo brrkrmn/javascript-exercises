3311a54e00be511970c0e07745fcee0eebf116ac
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-10 21:00:01 +0300

    solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    for (const value of values) {
+        let valueIndex = array.indexOf(value);
+        if (valueIndex == -1) {
+            continue;
+        } else {
+            array.splice(valueIndex, 1);
+        }
+    }
+    console.log(array);
+    return array;
 };
 
 // Do not edit below this line
