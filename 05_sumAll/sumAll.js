6133c257b5bb3262f96ce55a06c1033c74908b4d
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-10 21:37:06 +0300

    solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(value1, value2) {
+    let arrayLength, startingValue;
+    if (value1 < 0 || value2 < 0 || typeof value1 !== "number" || typeof value2 !== "number") {
+        return "ERROR";
+    } else if (value1 > value2) {
+        arrayLength = value1;
+        startingValue = value2;
+    } else {
+        arrayLength = value2;
+        startingValue = value1;
+    }
+    const array = [];
+    for (let i = startingValue; i <= arrayLength; i++) {
+        array.push(i);
+    }
+    let sum = 0;
+    for (const value of array) {
+        sum = sum + value;
+    }
+    console.log(array, sum);
+    return sum;
 };
 
 // Do not edit below this line
