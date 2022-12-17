1b857f5e337435afe096a5fe06fe34203902c9d5
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-17 20:13:04 +0300

    solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
-};
+const palindromes = function (string) {
+    string = string.toLowerCase();
+    string = string.replace(/\W/g, '');
+    let stringArray = string.split("");
+    if (stringArray.join('') === stringArray.reverse().join('')){
+        return true;
+    } else {
+        return false;
+    }
+    };
+    
 
 // Do not edit below this line
 module.exports = palindromes;
