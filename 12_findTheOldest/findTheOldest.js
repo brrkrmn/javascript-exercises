d31a8f1582e3a2f298827dde0b1472b020b7b16b
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-17 21:29:01 +0300

    solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = null;
+    people.forEach(human => {
+        if (!human.yearOfDeath) {
+            human.yearOfDeath = 2022;
+        };
+        if (oldest === null) {
+            oldest = human;
+        } else {
+            let currentAge = (human.yearOfDeath - human.yearOfBirth);
+            let oldestAge = (oldest.yearOfDeath - oldest.yearOfBirth);
+            if (currentAge > oldestAge) {
+                oldest = human;
+            }
+            console.log(currentAge, oldestAge);
+        }
+    })
+    return oldest;
 };
 
 // Do not edit below this line
