ff1ee983d292cfdb22977611acda09126dfd64c9
Berra Karaman <esma.berra.karaman@hotmail.com>
2022-12-17 20:51:25 +0300

    solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,13 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(targetIndex) {
+    const array = [1, 1];
+    if (targetIndex<0) {
+        return "OOPS";
+    };
+    for (let i = 2; i <= targetIndex; i++) {
+        array.push(array[array.length-1] + array[array.length-2]);
+    }
+    return(array[targetIndex-1]);
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
